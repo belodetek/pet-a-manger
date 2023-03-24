e89e56e352bbff90cdbc0358ed995a3e0f4137b3
belodetek <2033996+ab77@users.noreply.github.com>
2023-03-23 19:53:54 -0700

    update defaults
    change-type: patch

--- iwait/main.sh
+++ iwait/main.sh
@@ -8,11 +8,11 @@ function cleanup() {
 }
 trap cleanup EXIT
 
-IWAIT_MEAL_SCHEDULE=${IWAIT_MEAL_SCHEDULE:-15 2 6 * * *}
+IWAIT_MEAL_SCHEDULE=${IWAIT_MEAL_SCHEDULE:-21 2 6 * * *}
 IWAIT_MEAL_TRIGGER_CMD=${IWAIT_MEAL_TRIGGER_CMD:-/bin/bash -c \'pgrep python3.10 | xargs kill -s USR2\'}
 IWAIT_SLACK_ERRORS_ONLY=${IWAIT_SLACK_ERRORS_ONLY:-true}
 # semicolon separated cron expressions with commas replacing spaces
-IWAIT_SNACK_SCHEDULES=${IWAIT_SNACK_SCHEDULES:-42,32,12,*,*,*;24,01,19,*,*,*}
+IWAIT_SNACK_SCHEDULES=${IWAIT_SNACK_SCHEDULES:-42,2,12,*,*,*;19,1,17,*,*,*;4,4,22,*,*,*}
 IWAIT_SNACK_TRIGGER_CMD=${IWAIT_SNACK_TRIGGER_CMD:-/bin/bash -c \'pgrep python3.10 | xargs kill -s USR1\'}
 IWAIT_IFEED_CONTAINER_NAME=${IWAIT_IFEED_CONTAINER_NAME:-$(docker ps -q --filter 'name=ifeed*' --format '{{.Names}}')}
 
